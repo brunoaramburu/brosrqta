2df4b137dc7ab97ae2e62ec0594439263b75a8bd
brunoaramburu <brunoaranburu3@gmail.com>
2024-03-06 16:46:08 -0300

    host setting django

--- bros/settings.py
+++ bros/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3^zbaqz6g=gr0n7^#fm-pk(nml)=)^^su5%v$3gnjh3gae+%*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
