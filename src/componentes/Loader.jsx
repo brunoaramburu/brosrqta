878b9941371f2986cb510f825b6f86ec0b55dca2
brunoaramburu <brunoaranburu3@gmail.com>
2024-04-10 19:19:03 -0300

    Cambios front, toast 3

--- src/componentes/Loader.jsx
+++ src/componentes/Loader.jsx
@@ -8,8 +8,8 @@ const Loader = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-       }, 50);
-     //}, 6000);
+     //  }, 50);
+     }, 5000);
 
     return () => clearTimeout(timer);
   }, []);
